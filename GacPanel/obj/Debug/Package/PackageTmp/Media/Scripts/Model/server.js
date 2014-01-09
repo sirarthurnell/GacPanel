8ddf39b2e07c77a97824fe095ac8b3839e86f2cc
sirarthurnell <sirarthurnell@gmail.com>
2014-01-09 12:56:11 +0100

    Renombrado de la página principal a index.htm y cambiadas las rutas de las imágenes.

---
+++ GacPanel/obj/Debug/Package/PackageTmp/Media/Scripts/Model/server.js
@@ -0,0 +1,47 @@
+﻿/// <summary>
+/// Sirve de interfaz entre el servidor
+/// y el cliente.
+/// </summary>
+var server = (function () {
+
+    /// <summary>
+    /// Recupera la lista de directivas de enlace del
+    /// servidor.
+    /// </summary>
+    /// <param name="successCallback">Función a llamar
+    /// cuando se recupera la lista con éxito.</param>
+    /// <param name="failureCallback">Función a llamar
+    /// cuando falla la conexión con el servidor.</param>
+    function recoverDirectives(successCallback, failureCallback) {
+        var recoverPromise = $.post('Handlers/DirectivesListHandler.ashx');
+        recoverPromise.done(successCallback);
+        recoverPromise.fail(failureCallback);
+    }
+
+    /// <summary>
+    /// Aplica los cambios realizados en el servidor.
+    /// </summary>
+    /// <param name="data">Datos a enviar al servidor.</param>
+    /// <param name="successCallback">Función a llamar
+    /// cuando se aplican los cambios con éxito.</param>
+    /// <param name="failureCallback">Función a llamar
+    /// cuando falla la conexión con el servidor.</param>
+    function applyChanges(data, successCallback, failureCallback) {
+        var applyPromise = $.ajax({
+            url: 'Handlers/ApplyChangesHandler.ashx',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(data),
+            dataType: 'json'
+        });
+
+        applyPromise.done(successCallback);
+        applyPromise.fail(failureCallback);
+    }
+
+    return {
+        recoverDirectives: recoverDirectives,
+        applyChanges: applyChanges
+    };
+
+})();
---
+++ GacPanel/obj/Debug/Package/PackageTmp/Media/Scripts/Utils/templateLoader.js
@@ -0,0 +1,44 @@
+﻿/// <summary>
+/// Módulo que gestiona la carga de plantillas.
+/// </summary>
+var templateLoader = (function ($) {
+
+    /// <summary>
+    /// Carga la plantilla de lista de ensamblados a renderizar.
+    /// Después, inicia el renderizado.
+    /// </summary>
+    /// <param name="templates">Array de URLs de las plantillas
+    /// a cargar.</param>
+    /// <param name="callback">Función que será llamada una vez
+    /// se hayan cargado todas las plantillas.</param>
+    function loadTemplates(templates, callback) {
+        var downloadPromises = [],
+        allPromise;
+
+        $.each(templates, function (index, template) {
+            var templatePromise = $.get(template);
+            downloadPromises.push(templatePromise);
+        });
+
+        allPromise = $.when.apply(null, downloadPromises);
+        allPromise.done(function () {
+            var args = Array.prototype.slice.call(arguments, 0);
+
+            if (!_.isArray(args[0])) {
+                args = [args];
+            }
+
+            $.each(args, function (index, value) {
+                var templateHtml = value[0];
+                $('body').append(templateHtml);
+            });
+
+            callback();
+        });
+    }
+
+    return {
+        loadTemplates: loadTemplates
+    };
+
+})(jQuery);
