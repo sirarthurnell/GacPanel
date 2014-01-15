a8f54cc34d414facb5c633e8484f4e2c2eb8d96e
sirarthurnell <sirarthurnell@gmail.com>
2014-01-15 12:02:11 +0100

    Añadida pantalla de espera que se muestra en la primera carga de la aplicación.

---
+++ GacPanel/obj/Debug/Package/PackageTmp/Media/Scripts/upload.js
@@ -0,0 +1,32 @@
+﻿$(function () {
+
+    //Subida de archivos asíncrona.
+    $('#fileupload').fileupload({
+        dataType: 'json',
+        done: function (e, data) {
+
+            var result = data.result;
+
+            if (result.Success) {
+                alert(result.Data);
+            }
+
+            /*$.each(data.result.files, function (index, file) {
+            $('<p/>').text(file.name).appendTo(document.body);
+            });*/
+        }
+    });
+
+    //Soporte drag&drop.
+    $(document).on('dragenter', function (e) {
+
+    });
+
+    $(document).on('dragover', function (e) {
+
+    });
+
+    $(document).on('drop', function (e) {
+        $('#fileupload').trigger('fileupload');
+    });
+});
